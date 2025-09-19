5ce9d3a9699b681e4b2602498ce66b263a027df0
ifly0094-create <ifly0094@gmail.com>
2025-09-19 09:58:17 +0530

    fix errors

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const instance = axios.create({
-  baseURL:'http://localhost:8081',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8081',
 });
 // process.env.REACT_APP_API_BASE_URL
 // Request interceptor to add token to headers
